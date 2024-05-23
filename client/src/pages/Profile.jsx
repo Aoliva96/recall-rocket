14587052e132abee7703a93af3e554dc43298f2f
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-05-22 22:04:35 -0700

    Worked on Profile.jsx. Got the secret variable to work.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,65 +1,68 @@
-import { Navigate, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { Navigate, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import useDeviceType from "../components/useDeviceType";
 
-import ThoughtForm from '../components/ThoughtForm';
-import ThoughtList from '../components/ThoughtList';
+import ThoughtForm from "../components/ThoughtForm";
+import ThoughtList from "../components/ThoughtList";
 
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
 const Profile = () => {
-  const { username: userParam } = useParams();
+	const { username: userParam } = useParams();
+	const isMobile = useDeviceType();
 
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam },
-  });
+	const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+		variables: { username: userParam },
+	});
 
-  const user = data?.me || data?.user || {};
-  // navigate to personal profile page if username is yours
-  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Navigate to="/me" />;
-  }
+	const user = data?.me || data?.user || {};
+	// navigate to personal profile page if username is yours
+	if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+		return <Navigate to="/me" />;
+	}
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+	if (loading) {
+		return <div>Loading...</div>;
+	}
 
-  if (!user?.username) {
-    return (
-      <h4>
-        You need to be logged in to see this. Use the navigation links above to
-        sign up or log in!
-      </h4>
-    );
-  }
+	if (!user?.username) {
+		return (
+			<h4>
+				{isMobile
+					? "You need to be logged in to see this. Use the navigation links below to sign up or log in!"
+					: "You need to be logged in to see this. Use the navigation links above to sign up or log in!"}
+			</h4>
+		);
+	}
 
-  return (
-    <div>
-      <div className="flex-row justify-center mb-3">
-        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-        </h2>
+	return (
+		<div>
+			<div className="flex-row justify-center mb-3">
+				<h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
+					Viewing {userParam ? `${user.username}'s` : "your"} profile.
+				</h2>
 
-        <div className="col-12 col-md-10 mb-5">
-          <ThoughtList
-            thoughts={user.thoughts}
-            title={`${user.username}'s thoughts...`}
-            showTitle={false}
-            showUsername={false}
-          />
-        </div>
-        {!userParam && (
-          <div
-            className="col-12 col-md-10 mb-3 p-3"
-            style={{ border: '1px dotted #1a1a1a' }}
-          >
-            <ThoughtForm />
-          </div>
-        )}
-      </div>
-    </div>
-  );
+				<div className="col-12 col-md-10 mb-5">
+					<ThoughtList
+						thoughts={user.thoughts}
+						title={`${user.username}'s thoughts...`}
+						showTitle={false}
+						showUsername={false}
+					/>
+				</div>
+				{!userParam && (
+					<div
+						className="col-12 col-md-10 mb-3 p-3"
+						style={{ border: "1px dotted #1a1a1a" }}
+					>
+						<ThoughtForm />
+					</div>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
