4635ea541852618048bec7294958a5b69b202124
Aster Oliva <asterthealabaster@gmail.com>
2024-05-24 00:39:27 -0700

    added/modified seeders with card content and sample users

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,33 +1,51 @@
-const db = require('../config/connection');
-const { User, Thought } = require('../models');
-const userSeeds = require('./userSeeds.json');
-const thoughtSeeds = require('./thoughtSeeds.json');
-const cleanDB = require('./cleanDB');
+const db = require("../config/connection");
+const {
+  User,
+  MongoCard,
+  ExpressCard,
+  ReactCard,
+  NodeCard,
+} = require("../models");
+const cleanDB = require("./cleanDB");
+const mongoCardSeeds = require("./mongoCardSeeds.json");
+const expressCardSeeds = require("./expressCardSeeds.json");
+const reactCardSeeds = require("./reactCardSeeds.json");
+const nodeCardSeeds = require("./nodeCardSeeds.json");
+const userSeeds = require("./userSeeds.json");
 
-db.once('open', async () => {
+db.once("open", async () => {
   try {
-    await cleanDB('Thought', 'thoughts');
+    await cleanDB("MongoCard", "mongoCards");
+    await cleanDB("ExpressCard", "expressCards");
+    await cleanDB("ReactCard", "reactCards");
+    await cleanDB("NodeCard", "nodeCards");
+    await cleanDB("User", "users");
 
-    await cleanDB('User', 'users');
-
+    await MongoCard.create(mongoCardSeeds);
+    await ExpressCard.create(expressCardSeeds);
+    await ReactCard.create(reactCardSeeds);
+    await NodeCard.create(nodeCardSeeds);
     await User.create(userSeeds);
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
-        {
-          $addToSet: {
-            thoughts: _id,
-          },
-        }
-      );
-    }
+    // Commented code may not be needed for this project
+    // =================================================
+
+    // for (let i = 0; i < thoughtSeeds.length; i++) {
+    //   const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
+    //   const user = await User.findOneAndUpdate(
+    //     { username: thoughtAuthor },
+    //     {
+    //       $addToSet: {
+    //         thoughts: _id,
+    //       },
+    //     }
+    //   );
+    // }
   } catch (err) {
     console.error(err);
     process.exit(1);
   }
 
-  console.log('all done!');
+  console.log("Data seeded successfully");
   process.exit(0);
 });
