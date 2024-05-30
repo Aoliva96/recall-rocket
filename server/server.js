be01bfbde91bc76475d2bb7f96ceab49b26de468
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-05-29 19:40:18 -0700

    Changed casing on server.js of PORT to port.

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
 	typeDefs,
@@ -39,9 +39,9 @@ const startApolloServer = async () => {
 	}
 
 	db.once("open", () => {
-		app.listen(PORT, () => {
-			console.log(`🚀 API server running on port ${PORT}!`);
-			console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+		app.listen(port, () => {
+			console.log(`🚀 API server running on port ${port}!`);
+			console.log(`Use GraphQL at http://localhost:${port}/graphql`);
 		});
 	});
 };
