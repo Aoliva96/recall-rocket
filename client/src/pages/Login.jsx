9e43124273358f47ad38b00c662874e778f63001
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-05-30 22:47:48 -0700

    Working on connecting back with front.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,19 +4,20 @@ import { LOGIN_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
 
 const Login = () => {
-  const [formState, setFormState] = useState({ email: "", password: "" });
-  const [login, { error, data }] = useMutation(LOGIN_USER);
+	const [formState, setFormState] = useState({ email: "", password: "" });
+	const [login, { error, data }] = useMutation(LOGIN_USER);
 
-  // update state based on form input changes
-  const handleChange = (event) => {
-    const { name, value } = event.target;
+	// update state based on form input changes
+	const handleChange = (event) => {
+		const { name, value } = event.target;
 
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
+		setFormState({
+			...formState,
+			[name]: value,
+		});
+	};
 
+<<<<<<< HEAD
   // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -24,59 +25,68 @@ const Login = () => {
       const { data } = await login({
         variables: { ...formState },
       });
+=======
+	// submit form
+	const handleFormSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			const { data } = await login({
+				variables: { ...formState },
+			});
+>>>>>>> 28b3e95 (Working on connecting back with front.)
 
-      Auth.login(data.login.token);
-    } catch (e) {
-      console.error(e);
-    }
+			Auth.login(data.login.token);
+		} catch (e) {
+			console.error(e);
+		}
 
-    // clear form values
-    setFormState({
-      email: "",
-      password: "",
-    });
-  };
+		// clear form values
+		setFormState({
+			email: "",
+			password: "",
+		});
+	};
 
-  return (
-    <div className="card bg-white mt-2 w-50">
-      <h4 className="card-header bg-primary text-white p-2 pl-3">Login</h4>
-      <div className="card-body m-2">
-        {data ? (
-          <h1 className="m-5 p-5">&#10004;</h1>
-        ) : (
-          <form onSubmit={handleFormSubmit}>
-            <input
-              className="form-input mt-3"
-              placeholder="Your email"
-              name="email"
-              type="email"
-              value={formState.email}
-              onChange={handleChange}
-            />
-            <input
-              className="form-input mt-3"
-              placeholder="******"
-              name="password"
-              type="password"
-              value={formState.password}
-              onChange={handleChange}
-            />
-            <button
-              className="btn btn-block btn-primary mt-3"
-              style={{ cursor: "pointer" }}
-              type="submit"
-            >
-              Submit
-            </button>
-          </form>
-        )}
+	return (
+		<div className="card bg-white mt-2 w-50">
+			<h4 className="card-header bg-primary text-white p-2 pl-3">Login</h4>
+			<div className="card-body m-2">
+				{data ? (
+					<h1 className="m-5 p-5">&#10004;</h1>
+				) : (
+					<form onSubmit={handleFormSubmit}>
+						<input
+							className="form-input mt-3"
+							placeholder="Your email"
+							name="email"
+							type="email"
+							value={formState.email}
+							onChange={handleChange}
+						/>
+						<input
+							className="form-input mt-3"
+							placeholder="******"
+							name="password"
+							type="password"
+							value={formState.password}
+							onChange={handleChange}
+						/>
+						<button
+							className="btn btn-block btn-primary mt-3"
+							style={{ cursor: "pointer" }}
+							type="submit"
+						>
+							Submit
+						</button>
+					</form>
+				)}
 
-        {error && (
-          <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
-        )}
-      </div>
-    </div>
-  );
+				{error && (
+					<div className="my-3 p-3 bg-danger text-white">{error.message}</div>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Login;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,82 +6,107 @@ import Auth from "../utils/auth";
 import CardStack from "../components/CardStack";
 
 const Profile = () => {
-  const { username: userParam } = useParams();
-  // Check for token on component mount
-  useEffect(() => {
-    const token = Auth.getToken();
-  }, []);
+	const { username: userParam } = useParams();
+	// Check for token on component mount
+	useEffect(() => {
+		const token = Auth.getToken();
+	}, []);
 
-  // Check if user is logged in
-  if (!Auth.loggedIn()) {
-    return (
-      <div className="flex-row justify-center mb-4">
-        <h4 className="m-5 p-5">You must be logged in to view this page.</h4>
-      </div>
-    );
-  }
+	// Check if user is logged in
+	if (!Auth.loggedIn()) {
+		return (
+			<div className="flex-row justify-center mb-4">
+				<h4 className="m-5 p-5">You must be logged in to view this page.</h4>
+			</div>
+		);
+	}
 
-  // Fetch data for logged-in user
-  const { loading, data } = useQuery(QUERY_ME);
+	// Fetch data for logged-in user
+	const { loading, data } = useQuery(QUERY_ME);
 
-  // If no user data, return empty object
-  const user = data?.me || {};
+	// If no user data, return empty object
+	const user = data?.me || {};
 
-  // Check if loading
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+	// Check if loading
+	if (loading) {
+		return <div>Loading...</div>;
+	}
 
-  // Update user data
-  function updateUserData() {
-    // Update user data here
-  }
+	// Update user data
+	function updateUser() {
+		// Update user data here
+		console.log("Update button clicked");
+	}
 
-  return (
-    <div className="col-12 col-lg-10">
-      <div className="card bg-white mt-2">
-        <h4 className="card-header bg-primary text-white p-2 pl-3">
-          Welcome to your profile page, {user.username}!
-        </h4>
-        <div className="card-body mx-2">
-          <div className="display-flex align-center">
-            <h4>User Information</h4>
-          </div>
-          <hr className="mb-3" />
-          <p style={{ fontWeight: "bold" }}>Username: {user.username}</p>
-          <p style={{ fontWeight: "bold" }}>Email: {user.email}</p>
-          <button
-            className="btn btn-sm btn-primary text-white mt-1 mb-3 nav-btn"
-            onClick={updateUserData}
-          >
-            Update
-          </button>
-          <h4>Your Cards</h4>
-          <hr className="mb-3" />
-          {!user.cards ? (
-            <p>You haven't created any cards yet.</p>
-          ) : (
-            <p>User cards placeholder</p>
-            /* <CardStack cards={user.cards} /> */
-          )}
-          <Link
-            to="/cards"
-            className="btn btn-sm btn-primary text-white mt-1 mb-3"
-          >
-            Create a new card
-          </Link>
-          <h4>Your Favorites</h4>
-          <hr className="mb-3" />
-          {!user.cards ? (
-            <p>You haven't saved any favorites yet.</p>
-          ) : (
-            <p>User favorites placeholder</p>
-            /* <CardStack cards={user.favorites} /> */
-          )}
-        </div>
-      </div>
-    </div>
-  );
+	// Add an Admin owned card
+	function addAdminCard() {
+		// Add an admin card data here
+		console.log("Create a new admin card button clicked");
+	}
+
+	// Add a User owned card
+	function addUserCard() {
+		// Add user card data here
+		console.log("Create a new card button clicked");
+	}
+
+	return (
+		<div className="col-12 col-lg-10">
+			<div className="card bg-white mt-2">
+				<h4 className="card-header bg-primary text-white p-2 pl-3">
+					Welcome to your profile page, {user.username}!
+				</h4>
+				<div className="card-body mx-2">
+					<div className="display-flex align-center">
+						<h4>User Information</h4>
+					</div>
+					<hr className="mb-3" />
+					<p style={{ fontWeight: "bold" }}>Username: {user.username}</p>
+					<p style={{ fontWeight: "bold" }}>Email: {user.email}</p>
+					<button
+						className="btn btn-sm btn-primary text-white mt-1 mb-3 nav-btn"
+						onClick={updateUser}
+					>
+						Update
+					</button>
+					<h4>Your Cards</h4>
+					<hr className="mb-3" />
+					{!user.cards ? (
+						<p>You haven't created any cards yet.</p>
+					) : (
+						<p>User cards placeholder</p>
+						/* <CardStack cards={user.cards} /> */
+					)}
+					{!user.admin && (
+						<button
+							to="/cards"
+							className="btn btn-sm btn-primary text-white mt-1 mb-3"
+							onClick={addUserCard}
+						>
+							Create a new card
+						</button>
+					)}
+					{user.admin && (
+						<button
+							to="/cards"
+							className="btn btn-sm btn-secondary text-white mt-1 mb-3"
+							onClick={addAdminCard}
+						>
+							Create a new admin card
+						</button>
+					)}
+					<h4>Your Favorites</h4>
+					<hr className="mb-3" />
+					{!user.cards ? (
+						<p>You haven't saved any favorites yet.</p>
+					) : (
+						<p>User favorites placeholder</p>
+						/* <CardStack cards={user.favorites} /> */
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
