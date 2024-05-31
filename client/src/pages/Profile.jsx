2d5ce7357817c04d96d95bbf9fb8c3de9a737d78
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-05-30 19:18:51 -0700

    hotfix for filepath for CardStack in Profile.jsx.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,10 +3,9 @@ import { useQuery } from "@apollo/client";
 import { QUERY_ME } from "../utils/queries";
 import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
-import CardStack from "../components/CardStack/index";
+import CardStack from "../components/CardStack";
 
 const Profile = () => {
-
 	// Check for token on component mount
 	useEffect(() => {
 		const token = Auth.getToken();
