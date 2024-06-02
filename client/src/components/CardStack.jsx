7e7eed055eb60f47a951e0f0dea8e08b8781f6fe
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-06-01 20:48:52 -0700

    rebased.

--- client/src/components/CardStack.jsx
+++ client/src/components/CardStack.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { QUERY_CARDS } from "../utils/queries";
+import { ADD_FAVORITE, REMOVE_FAVORITE } from "../utils/mutations";
 
 const CardStack = ({ cards = [] }) => {
 	const { concept: urlConcept } = useParams(); // Get concept from URL params
 
 	const [currentIndex, setCurrentIndex] = useState(0);
 	const [showAnswer, setShowAnswer] = useState(false);
-	// const [localCards, setLocalCards] = useState([]);
+	const [addFavorite] = useMutation(ADD_FAVORITE);
+	const [removeFavorite] = useMutation(REMOVE_FAVORITE);
 
 	const createdByIds = cards.map((card) => card.createdBy.id);
 
@@ -29,7 +31,7 @@ const CardStack = ({ cards = [] }) => {
 
 	const handleBack = () => {
 		setCurrentIndex(
-			(prevIndex) => (prevIndex - 1 + localCards.length) % cards.length
+			(prevIndex) => (prevIndex - 1 + cards.length) % cards.length
 		);
 		setShowAnswer(false);
 	};
@@ -39,6 +41,52 @@ const CardStack = ({ cards = [] }) => {
 	};
 
 	const card = cards[currentIndex];
+
+	const handleAddFavorite = async () => {
+		try {
+			console.log("Adding to favorites...");
+			console.log("Card ID:", card._id);
+			const result = await addFavorite({
+				variables: {
+					cardId: card._id,
+				},
+				refetchQueries: [
+					{
+						query: QUERY_CARDS,
+						variables: { concept: urlConcept, createdBy: createdByIds },
+					},
+				],
+			});
+		} catch (error) {
+			console.error("Error adding favorite:", error);
+		}
+	};
+
+	const handleRemoveFavorite = async (favoriteId) => {
+		try {
+			await removeFavorite({
+				variables: {
+					favoriteId,
+				},
+				refetchQueries: [
+					{
+						query: QUERY_CARDS,
+						variables: { concept: urlConcept, createdBy: createdByIds },
+					},
+				],
+			});
+		} catch (error) {
+			console.error("Error removing favorite:", error);
+		}
+	};
+
+	const isFavorite = (favoriteId) => {
+		return (
+			card &&
+			card.favorites &&
+			card.favorites.some((fav) => fav._id === favoriteId)
+		);
+	};
 
 	return (
 		<div className="my-3">
@@ -62,8 +110,18 @@ const CardStack = ({ cards = [] }) => {
 					{showAnswer ? card.answer : "Click to reveal the answer"}
 				</blockquote>
 			</div>
+
 			<button onClick={handleBack}>Back</button>
 			<button onClick={handleNext}>Next</button>
+			<div>
+				{!isFavorite(card._id) ? (
+					<button onClick={handleAddFavorite}>Add to Favorites</button>
+				) : (
+					<button onClick={() => handleRemoveFavorite(card._id)}>
+						Remove from Favorites
+					</button>
+				)}
+			</div>
 		</div>
 	);
 };
