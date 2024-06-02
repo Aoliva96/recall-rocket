4c1cc30d9a07a4fda26eb0603daced4384d091a6
Aster Oliva <asterthealabaster@gmail.com>
2024-06-02 06:05:47 -0700

    added react-card-flip package to client, styled cards

--- client/src/components/CardCarousel.jsx
+++ client/src/components/CardCarousel.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import ExpandableNav from "../components/ExpandableNav";
+import ReactCardFlip from "react-card-flip";
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { QUERY_CARDS } from "../utils/queries";
@@ -6,7 +8,6 @@ import { QUERY_CARDS } from "../utils/queries";
 const CardCarousel = ({ cards, concept }) => {
   const { concept: urlConcept } = useParams();
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [showAnswer, setShowAnswer] = useState(false);
   const [localCards, setLocalCards] = useState([]);
 
   const { loading, data, error } = useQuery(QUERY_CARDS, {
@@ -27,12 +28,20 @@ const CardCarousel = ({ cards, concept }) => {
     };
   }, []);
 
+  // React card flip
+  const [isFlipped, setIsFlipped] = useState(false);
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  // Set card content
   useEffect(() => {
     if (data) {
       setLocalCards(data.cards);
     }
   }, [loading, data, error, urlConcept]);
 
+  // Check for loading or no cards
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -40,22 +49,17 @@ const CardCarousel = ({ cards, concept }) => {
     return <div>No cards found.</div>;
   }
 
+  // Handle next and back buttons
   const handleNext = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % localCards.length);
-    setShowAnswer(false);
+    setIsFlipped(false);
   };
-
   const handleBack = () => {
     setCurrentIndex(
       (prevIndex) => (prevIndex - 1 + localCards.length) % localCards.length
     );
-    setShowAnswer(false);
+    setIsFlipped(false);
   };
-
-  const toggleAnswer = () => {
-    setShowAnswer((prevShowAnswer) => !prevShowAnswer);
-  };
-
   const card = localCards[currentIndex];
 
   // Format concept name for display
@@ -70,41 +74,46 @@ const CardCarousel = ({ cards, concept }) => {
 
   return (
     <>
-      <div className="col-12 col-lg-10">
-        <div className="card bg-primary text-white text-center mt-2 px-0 pt-2 pb-1 col-6">
+      <div className={isDesktop ? "col-6" : "col-12"}>
+        {/* Card concept */}
+        <div className="card bg-primary text-white text-center mt-2 px-0 pt-2 pb-1">
           <h4>{cardConcept}</h4>
         </div>
-        <div
-          className={
-            isDesktop
-              ? "card bg-white text-center mt-2 p-0 col-6"
-              : "card bg-white text-center mt-2 p-0 col-12"
-          }
-        >
-          <h4 className="card-header bg-primary text-white p-2 pl-3">
-            {card.question}
-          </h4>
-          <div className="card-body m-2">
-            <blockquote
-              onClick={toggleAnswer}
-              className="p-4"
-              style={{
-                fontSize: "1.5rem",
-                fontStyle: "italic",
-                border: "2px dotted #1a1a1a",
-                lineHeight: "1.5",
-              }}
-            >
-              {showAnswer ? card.answer : "Click to reveal the answer"}
-            </blockquote>
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+          {/* Card front */}
+          <div
+            onClick={handleFlip}
+            className="card bg-white text-center mt-2 p-0"
+          >
+            <h5 className="card-header bg-primary text-white p-2 pl-3">
+              {card.question}
+            </h5>
+            <div className="p-4 m-3 bg-white btn btn-white flip-content">
+              <span style={{ fontStyle: "italic" }}>Click to show answer</span>
+            </div>
           </div>
+          {/* Card back */}
+          <div
+            onClick={handleFlip}
+            className="card bg-white text-center mt-2 p-0"
+          >
+            <h5 className="card-header bg-primary text-white p-2 pl-3">
+              {card.question}
+            </h5>
+            <div className="p-4 m-3 bg-white btn btn-white flip-content">
+              {card.answer}
+            </div>
+          </div>
+        </ReactCardFlip>
+        {/* Card nav buttons */}
+        <div className="card bg-primary text-white text-center mt-2 p-0">
           <div className="w-100 text-center">
             <button
               onClick={handleBack}
               className="btn btn-lg btn-primary w-50"
               style={{
-                borderRadius: "0 0 0 5px",
                 borderRight: "1px solid slateGray",
+                borderRadius: "5px 0 0 5px",
               }}
             >
               &#9666; Back
@@ -112,13 +121,18 @@ const CardCarousel = ({ cards, concept }) => {
             <button
               onClick={handleNext}
               className="btn btn-lg btn-primary w-50"
-              style={{ borderRadius: "0 0 5px 0" }}
+              style={{
+                borderLeft: "1px solid slateGray",
+                borderRadius: "0 5px 5px 0",
+              }}
             >
               Next &#9656;
             </button>
           </div>
         </div>
       </div>
+      {/* ExpandableNav for mobile */}
+      <ExpandableNav />
     </>
   );
 };
